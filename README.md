210e5fae4432eae270cc0c8fb2998a8c0684e409
Michel Medema <michel.medema@gmail.com>
2025-08-29 10:46:24 +0200

    Added a README with information about the classes in this repository.

---
+++ README.md
@@ -0,0 +1,37 @@
+# Scala Graph Tools
+
+This repository contains a small collection of graph algorithms and data structures.
+
+## `nl.rug.ds.common`
+
+Helper methods to deal with common tasks such as removing extensions from filenames and writing to files.
+
+## `nl.rug.ds.graph.common`
+
+Data structures for graphs and common graph functions, such as for computing the components of a graph and finding cliques.
+
+## `nl.rug.ds.graph.communities`
+
+An implementation of the Louvain algorithm that computes the communities of a graph. It is described in the paper
+
+>V. D. Blondel, J.-L. Guillaume, R. Lambiotte, and E. Lefebvre, ‘Fast unfolding of communities in large networks’, J. Stat. Mech., vol. 2008, no. 10, p. P10008, Oct. 2008, doi: 10.1088/1742-5468/2008/10/P10008.
+
+## `nl.rug.ds.graph.format.parser`
+
+The object in `Pace.scala` reads a graph from a file or writes it to a file following the [PACE format](https://github.com/PACE-challenge/Treewidth?tab=readme-ov-file#input-format). The methods in `SeparatorFile.scala` can be used to read and write the vertex separators of a graph. This is a convenience class used in combination with the class `nl.rug.ds.graph.generator.SafeSeparatorGraph`.
+
+## `nl.rug.ds.graph.generator`
+
+A generator to create random graphs and a generator to create graphs with vertex separators that are known to be safe. Safe separators were first introduced in the paper
+
+> H. L. Bodlaender and A. M. C. A. Koster, ‘Safe separators for treewidth’, Discrete Mathematics, vol. 306, no. 3, pp. 337–350, Feb. 2006, doi: 10.1016/j.disc.2005.12.017.
+
+## `nl.rug.ds.graph.tree.decomposition`
+
+`PaceTD.scala` provides a parser to read a tree decomposition from a file that is encoded using the [PACE format](https://github.com/PACE-challenge/Treewidth?tab=readme-ov-file#output-format). `Tamaki.scala` is a wrapper around [the treewidth algorithm of Hisao Tamaki](https://github.com/twalgor/tw). This algorithm has been implemented in Java. One of the GitHub workflows of this repository packages the algorithm and publishes it to the GitHub package registry, allowing it to be used directly in this project.
+
+## `nl.rug.ds.graph.triangulation`
+
+Data structures to store triangulated graphs and algorithms to produce triangulated graphs. Currently, two common heuristic triangulation algorithms are provided: min-fill and min-degree. This package also contains an implementation of the Heuristic Decomposition with Community Structure algorithm, which decomposes a graph using the boundaries of the communities as likely safe separators. This algorithm is described in the paper 
+
+> M. Medema and A. Lazovik, ‘A safeness condition for minimal separators based on vertex connectivity’, Discrete Mathematics, vol. 348, no. 9, p. 114524, Sep. 2025, doi: 10.1016/j.disc.2025.114524.
--- src/main/scala/nl/rug/ds/common/FileHelper.scala
+++ src/main/scala/nl/rug/ds/common/FileHelper.scala
@@ -1,49 +1,27 @@
 package nl.rug.ds.common
 
 import java.io.{BufferedWriter, File, FileWriter}
-import java.nio.file.{Files, Path}
-import scala.annotation.tailrec
-import scala.collection.immutable.Queue
-import scala.jdk.CollectionConverters.IteratorHasAsScala
-import scala.util.{Failure, Success, Try, Using}
+import java.nio.file.Path
+import scala.util.{Try, Using}
 
 trait FileHelper {
 	/**
-		*
 		* Returns the name of the file to which this path points without its extension.
 		*/
 	def getFileName( path: Path ): String = {
 		stripExtension(path.getFileName.toString)
 	}
 
-	def listFiles( path: Path ): Try[LazyList[Path]] = Try( Files.list(path).iterator().asScala.to(LazyList) )
-
-	def stripTrailingSeparator(s: String): String = {
-		val end: Int = if (s.endsWith(File.separator)) s.length - File.separator.length else s.length
-		s.substring( 0, end )
-	}
-
-	def stripLeadingSeparator(s: String): String = {
-		val start: Int = if (s.startsWith(File.separator)) File.separator.length else 0
-		s.substring( start )
-	}
-
+  /**
+   * Returns the filename without the extension. Note that this simple implementation
+   * does not always correctly identify the extension: it, for instance, returns the
+   * empty string for dot files.
+   */
 	def stripExtension(fileName: String): String = {
 		fileName.lastIndexOf('.') match {
 			case -1 => fileName
 			case idx => fileName.substring(0, idx)
 		}
-	}
-
-	def buildPath( s: String, paths: String* ): String = {
-		stripTrailingSeparator(s) ++ paths.map( s => stripTrailingSeparator(stripLeadingSeparator(s)) ).mkString(File.separator, File.separator, File.separator)
-	}
-
-	def buildFilePath( s: String, paths: String* ): String = {
-		stripTrailingSeparator(s) ++
-			File.separator ++
-			stripLeadingSeparator(paths.init.map( s => stripTrailingSeparator(stripLeadingSeparator(s)) ).mkString("", File.separator, File.separator)) ++
-			stripTrailingSeparator(stripLeadingSeparator(paths.last))
 	}
 
 	def writeToFile(file: File, content: String): Try[Unit] = writeToFile(file, Iterator(content))
@@ -52,37 +30,5 @@ trait FileHelper {
 		Using( new BufferedWriter( new FileWriter( file ) ) ) { writer =>
 			lines.foreach( line => writer.write(line ++ "\n") )
 		}
-	}
-
-	def createDirectories(path: String): Boolean = {
-		createDirectories( new File(path) )
-	}
-
-	def createDirectories(file: File): Boolean = {
-		file.mkdirs()
-	}
-
-	def listFiles(dir: File, recursive: Boolean = false, filter: String => Boolean = _ => true ): Try[List[File]] = {
-		@tailrec
-		def processDirs(dirs: List[File], files: Queue[File]): Try[List[File]] = {
-			dirs match {
-				case Nil => Success(files.toList)
-				case dir :: tail =>
-					Try {
-						dir.listFiles().toList.filter( file => filter(file.getName) ).partition( _.isFile )
-					} match {
-						case Success( t: (List[File], List[File]) ) =>
-							if ( recursive ) {
-								processDirs( tail prependedAll t._2, files ++ t._1 )
-							} else {
-								processDirs( tail, files ++ t._1 )
-							}
-
-						case Failure( e: Throwable ) => Failure( e )
-					}
-			}
-		}
-
-		processDirs(List(dir), Queue.empty)
 	}
 }
